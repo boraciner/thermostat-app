541e23e73bbb5f8d2b0546d5f5a90aa454efc2d1
Bora Ciner <bora.ciner@audivo.com>
2021-03-24 22:29:20 +0100

    Days Screen improvement

--- screens/ConnectDeviceScreen.js
+++ screens/ConnectDeviceScreen.js
@@ -1,66 +1,68 @@
-import {Text,View, StyleSheet, Button} from 'react-native';
-import React , {useState} from 'react'
+import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
 
-import DeviceList from '../components/DeviceList'
-let myCounter = 1
-
-function onConnectDevice(value){
-    console.log("Connect to Device : " , value)    
-}
+import DeviceList from "../components/DeviceList";
+let myCounter = 1;
 
 
-const ConnectDeviceScreen = props => {
-    const[scanButtonTitle,setScanButtonTitle] = useState('Scan BLE Devices')
-    const[isDisplayDeviceList,setIsDisplayDeviceList] = useState(false)
-    const[foundDevices,setFoundDevices] = useState([])
 
-    
-    
+const ConnectDeviceScreen = (props) => {
 
-    const ScanBLEDevices = () =>{
-        if(scanButtonTitle === 'Scan BLE Devices')
-        {
-            setFoundDevices([...foundDevices,{id: String(myCounter++) , name: 'Bluetooh Device '+myCounter}])
-            setIsDisplayDeviceList(true)
-            setScanButtonTitle('Stop')
-        }
-        else{
-            setIsDisplayDeviceList(false)
-            setScanButtonTitle('Scan BLE Devices')
-        }
+
+  const [scanButtonTitle, setScanButtonTitle] = useState("Scan BLE Devices");
+  const [isDisplayDeviceList, setIsDisplayDeviceList] = useState(false);
+  const [foundDevices, setFoundDevices] = useState([]);
+
+
+  onConnectDevice = (value) => {
+    console.log("Connect to Device : ", value);
+  
+    props.navigation.navigate(
+      {   routeName: "Days",
+          params: {
+              device: String(value)
+          }
+      }
+      );
+  }
+
+
+  const ScanBLEDevices = () => {
+    if (scanButtonTitle === "Scan BLE Devices") {
+      setFoundDevices([
+        ...foundDevices,
+        { id: String(myCounter++), name: "Bluetooh Device " + myCounter },
+      ]);
+      setIsDisplayDeviceList(true);
+      setScanButtonTitle("Stop");
+    } else {
+      setIsDisplayDeviceList(false);
+      setScanButtonTitle("Scan BLE Devices");
     }
+  };
 
-    return(
-        <View style={styles.screen}>
-            <Text style={styles.descriptionText}>Connect Device Screen</Text>
-            <View style={styles.buttonContainer}>
-                <Button title={scanButtonTitle} onPress={ScanBLEDevices}/>
-            </View>
-            {isDisplayDeviceList ? <DeviceList devices={foundDevices} onConnect={onConnectDevice} /> : null}
-            <View style={styles.buttonContainer}>
-            <Button title="Go to Days" style={styles.button} onPress={()=>{props.navigation.navigate('Days')}}/>
-            </View>
-
-        </View>
-        
-    );
-
-}
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.descriptionText}>Connect Device Screen</Text>
+      <View style={styles.buttonContainer}>
+        <Button title={scanButtonTitle} onPress={ScanBLEDevices} />
+      </View>
+      {isDisplayDeviceList ? (
+        <DeviceList devices={foundDevices} onConnect={onConnectDevice} />
+      ) : null}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        alignItems: 'center',
-        
-    },
-    descriptionText:{
-        fontSize: 25,
-        margin: 20
-    },
-    buttonContainer:{
-        margin: 20
-    }
-
+  screen: {
+    flex: 1,
+    alignItems: "center",
+  },
+  descriptionText: {
+    fontSize: 25,
+    margin: 20,
+  },
 });
 
 export default ConnectDeviceScreen;
--- screens/DaysScreen.js
+++ screens/DaysScreen.js
@@ -1,17 +1,53 @@
-import {Text,View, StyleSheet} from 'react-native';
-import React from 'react'
+import {Text,View, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+
+const Day = props => {
+    return(
+        <TouchableOpacity onPress={()=>{props.onSelect(props.title)}} style={styles.dayContainer}>
+        <View>
+            <Text>{props.title} is here</Text>
+        </View>
+        </TouchableOpacity>
+    )
+}
 const DaysScreen = props => {
+    const deviceName = props.navigation.getParam('device');
+    const [selectedDay,setSelectedDay] = useState()
+    const showDayDetail = day => {
+        console.log("selected day ",day)
+    }
 
     return(
-        <View>
-            <Text>Days Screen</Text>
+        <View style={{flex: 1}}>
+            <Text>{deviceName} Thermometer Configuration</Text>
+            <Text>Current Temperature is 23 C.</Text>
+            <View style={styles.weekContainer}>
+                <Day title="Monday" onSelect={showDayDetail}></Day>
+                <Day title="Tuesday" onSelect={showDayDetail}></Day>
+                <Day title="Wednesday" onSelect={showDayDetail}></Day>
+                <Day title="Thursday" onSelect={showDayDetail}></Day>
+                <Day title="Friday" onSelect={showDayDetail}></Day>
+                <Day title="Saturday" onSelect={showDayDetail}></Day>
+                <Day title="Sunday" onSelect={showDayDetail}></Day>
+            </View>
+            
         </View>
     );
 
 }
 
 const styles = StyleSheet.create({
-    
+    weekContainer:{
+        flex: 1,
+        backgroundColor: 'yellow'
+    },
+    dayContainer:{
+        flex: 1,
+        marginVertical: 10,
+        backgroundColor: 'green'
+
+    },
+
 });
 
 export default DaysScreen;
